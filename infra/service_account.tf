ae375812ec682abafe4ce122ac0247d12304e5b8
Sharma <lsharma3@de-wtkh4t230r.atrema.deloitte.com>
2023-11-08 14:35:47 +0100

    make module reachable for terraform code

--- infra/service_account.tf
+++ infra/service_account.tf
@@ -1,5 +1,5 @@
 module "sa" {
-  source             = "../../modules/iam/service_account"
+  source             = "../modules/iam/service_account"
   service_account_id = "cloudrun-sa-test"
   display_name       = "Service Account created by terraform"
   sa_role            = toset(var.sa_role)
